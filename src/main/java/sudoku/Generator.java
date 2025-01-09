e9c06f97299175f413f7bc0342be38a778d52207
Caio Reis <caiofreis2005@gmail.com>
2025-01-08 22:06:34 -0300

    Faz alteração na documentação da função generateRandomBoard

--- src/main/java/sudoku/Generator.java
+++ src/main/java/sudoku/Generator.java
@@ -1,20 +1,25 @@
 package sudoku;
 import java.util.Random;
+
 /**
- * Gera valores aleatórios para preencher um tabuleiro de Sudoku.
- * 
- * Esta função insere um número especificado de valores aleatórios em posições 
- * vazias (células com valor 0) de um tabuleiro de Sudoku. Os valores são gerados 
- * aleatoriamente entre 1 e 9, e cada tentativa garante que uma célula vazia seja preenchida.
- * 
- * @param board o tabuleiro de Sudoku a ser preenchido.
- *              Deve ser uma instância da classe {@code Board}, que permite definir e obter valores das células.
- * @param num o número de valores aleatórios a serem gerados e inseridos no tabuleiro.
- * 
- * @throws NullPointerException se o parâmetro {@code board} for {@code null}.
- * @throws IllegalArgumentException se o número de valores a serem inseridos {@code num} for negativo.
+ *
+ * @author caiofdev
  */
 public class Generator {
+    /**
+     * Gera valores aleatórios para preencher um tabuleiro de Sudoku.
+     * 
+     * Esta função insere um número especificado de valores aleatórios em posições 
+     * vazias (células com valor 0) de um tabuleiro de Sudoku. Os valores são gerados 
+     * aleatoriamente entre 1 e 9, e cada tentativa garante que uma célula vazia seja preenchida.
+     * 
+     * @param board o tabuleiro de Sudoku a ser preenchido.
+     *              Deve ser uma instância da classe {@code Board}, que permite definir e obter valores das células.
+     * @param num o número de valores aleatórios a serem gerados e inseridos no tabuleiro.
+     * 
+     * @throws NullPointerException se o parâmetro {@code board} for {@code null}.
+     * @throws IllegalArgumentException se o número de valores a serem inseridos {@code num} for negativo.
+     */
     public static void generateRandomBoard(Board board, int num) {
         Random rand = new Random();
         for (int i = 0; i < num; i++) {
