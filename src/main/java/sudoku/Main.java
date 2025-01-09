59ef1d10d617cdb62708d0a1a978a0aae8e46055
Caio Reis <caiofreis2005@gmail.com>
2025-01-08 22:21:13 -0300

    Faz a função principal da aplicação

--- src/main/java/sudoku/Main.java
+++ src/main/java/sudoku/Main.java
@@ -1,7 +1,19 @@
 package sudoku;
 
 public class Main {
+/**
+ * Método principal da aplicação.
+ * 
+ * Este método é o ponto de entrada da aplicação. Ele cria uma instância da classe {@code Menu} 
+ * e exibe a tela de boas-vindas utilizando o método {@code displayWelcomeScreen()}.
+ * 
+ * O método principal organiza a execução inicial da aplicação, preparando o ambiente 
+ * para interações posteriores com o usuário.
+ * 
+ * @param args argumentos de linha de comando (não utilizados neste caso).
+ */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Menu menu = new Menu();
+        menu.displayWelcomeScreen();
     }
 }
